91cd58ea871257f6e931e506fee4c2333bb60a3f
Hewlett <yhwi.kwon@gmail.com>
2025-03-05 02:27:14 +0900

    apply black formatter

--- test_diff_estimation.py
+++ test_diff_estimation.py
@@ -8,27 +8,27 @@ from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_sc
 
 def get_review_feedback(model, patch, language):
     # OpenAI API 키 설정
-    with open('gpt.key', 'r') as key_file:
+    with open("gpt.key", "r") as key_file:
         api_key = key_file.readline().strip()
     openai.api_key = api_key
 
     diff_text = patch
-    prompt = open('prompt/diff_estimation_prompt.txt').read()
-    cur_prompt = prompt.replace('{{diff_text}}', diff_text)
+    prompt = open("prompt/diff_estimation_prompt.txt").read()
+    cur_prompt = prompt.replace("{{diff_text}}", diff_text)
 
     language_map = {
-        'go': 'Go',
-        'java': 'Java',
-        'py': 'Python',
-        'cpp': 'C++',
-        'js': 'JavaScript',
-        'rb': 'Ruby',
-        'cs': 'C#',
-        'c': 'C',
-        'php': 'PHP'
+        "go": "Go",
+        "java": "Java",
+        "py": "Python",
+        "cpp": "C++",
+        "js": "JavaScript",
+        "rb": "Ruby",
+        "cs": "C#",
+        "c": "C",
+        "php": "PHP",
     }
-    language = language_map.get(language, '')
-    cur_prompt = cur_prompt.replace('{{language}}', language)
+    language = language_map.get(language, "")
+    cur_prompt = cur_prompt.replace("{{language}}", language)
 
     # ChatGPT API에 질문하여 코드리뷰 필요 여부 판단
     response = openai.ChatCompletion.create(
@@ -36,25 +36,25 @@ def get_review_feedback(model, patch, language):
         messages=[
             # Changed 'system' to 'user'
             {"role": "user", "content": cur_prompt}
-        ]
+        ],
     )
 
     # ChatGPT의 응답 내용 추출
-    answer = response['choices'][0]['message']['content'].strip()
+    answer = response["choices"][0]["message"]["content"].strip()
 
     # bold markdown 제거
-    answer = answer.replace('**', '')
+    answer = answer.replace("**", "")
 
     # Regular expression to extract the number
     match = (
-        re.search(r'Code Review Required: (\d)', answer) or
-        re.search(r'Code Review Required (1-5): (\d)', answer) or
-        re.search(r'Final Evaluation: (\d)', answer) or
-        re.search(r'Final Evaluation (1-5): (\d)', answer) or
-        re.search(r'Final Score: (\d)', answer) or
-        re.search(r'Final Evaluation Score: (\d)', answer) or
-        re.search(r'Final Evaluation (overall): (\d)', answer) or
-        re.search(r'Final Evaluation (1-5 scores ONLY): (\d)', answer)
+        re.search(r"Code Review Required: (\d)", answer)
+        or re.search(r"Code Review Required (1-5): (\d)", answer)
+        or re.search(r"Final Evaluation: (\d)", answer)
+        or re.search(r"Final Evaluation (1-5): (\d)", answer)
+        or re.search(r"Final Score: (\d)", answer)
+        or re.search(r"Final Evaluation Score: (\d)", answer)
+        or re.search(r"Final Evaluation (overall): (\d)", answer)
+        or re.search(r"Final Evaluation (1-5 scores ONLY): (\d)", answer)
     )
 
     # Extract and print the result if found
@@ -66,14 +66,12 @@ def get_review_feedback(model, patch, language):
         print(answer)
 
     # Additional score parsing
-    significance_match = re.search(r'Code Change Significance: (\d)', answer)
-    complexity_match = re.search(r'Complexity of Changes: (\d)', answer)
-    consistency_match = re.search(
-        r'Code Consistency and Readability: (\d)', answer)
-    risks_match = re.search(r'Potential Risks or Issues: (\d)', answer)
+    significance_match = re.search(r"Code Change Significance: (\d)", answer)
+    complexity_match = re.search(r"Complexity of Changes: (\d)", answer)
+    consistency_match = re.search(r"Code Consistency and Readability: (\d)", answer)
+    risks_match = re.search(r"Potential Risks or Issues: (\d)", answer)
 
-    significance = int(significance_match.group(1)
-                       ) if significance_match else 3
+    significance = int(significance_match.group(1)) if significance_match else 3
     complexity = int(complexity_match.group(1)) if complexity_match else 2
     consistency = int(consistency_match.group(1)) if consistency_match else 4
     risks = int(risks_match.group(1)) if risks_match else 1
@@ -82,18 +80,19 @@ def get_review_feedback(model, patch, language):
 
 
 def evaluate_patch(model, patch):
-    y_true = int(patch['y'])
-    language = patch.get('lang', '')
-    review_needed, score, significance, complexity, consistency, risks = get_review_feedback(
-        model, patch['patch'], language)
+    y_true = int(patch["y"])
+    language = patch.get("lang", "")
+    review_needed, score, significance, complexity, consistency, risks = (
+        get_review_feedback(model, patch["patch"], language)
+    )
     y_pred = int(review_needed)
 
-    patch['y_pred'] = review_needed
-    patch['y_pred_score'] = score
-    patch['y_code_change_significance'] = significance
-    patch['y_complexity_of_changes'] = complexity
-    patch['y_code_consistency_and_readability'] = consistency
-    patch['y_potential_risks_or_issues'] = risks
+    patch["y_pred"] = review_needed
+    patch["y_pred_score"] = score
+    patch["y_code_change_significance"] = significance
+    patch["y_complexity_of_changes"] = complexity
+    patch["y_code_consistency_and_readability"] = consistency
+    patch["y_potential_risks_or_issues"] = risks
 
     return y_true, y_pred, patch
 
@@ -116,13 +115,16 @@ def save_metrics(y_true_list, y_pred_list, input_file_name, model, current_time)
         "Precision": precision,
         "Recall": recall,
         "F1 Score": f1,
-        "Accuracy": accuracy
+        "Accuracy": accuracy,
     }
 
-    with open(f'output/1.1/{input_file_name}_{model}_{current_time}.result', 'w') as result_file:
+    with open(
+        f"output/1.1/{input_file_name}_{model}_{current_time}.result", "w"
+    ) as result_file:
         json.dump(result, result_file, indent=4)
     print(
-        f"Metrics saved to output/1.1/{input_file_name}_{model}_{current_time}.result")
+        f"Metrics saved to output/1.1/{input_file_name}_{model}_{current_time}.result"
+    )
 
 
 def main():
@@ -134,14 +136,16 @@ def main():
     # model = "o3-mini"
     # model = "o1-mini"
 
-    input_file_name = 'diff_estimation_sampling_100(seed42).jsonl'
+    input_file_name = "diff_estimation_sampling_100(seed42).jsonl"
 
-    with open(f'data/{input_file_name}', 'r') as file:
+    with open(f"data/{input_file_name}", "r") as file:
         patches = [json.loads(line) for line in file]
 
-    current_time = datetime.now().strftime('%Y%m%d_%H%M%S')
+    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-    with open(f'output/1.1/{input_file_name}_{model}_{current_time}.jsonl', 'w') as output_file:
+    with open(
+        f"output/1.1/{input_file_name}_{model}_{current_time}.jsonl", "w"
+    ) as output_file:
 
         y_true_list = []
         y_pred_list = []
@@ -152,11 +156,10 @@ def main():
                 y_true_list.append(y_true)
                 y_pred_list.append(y_pred)
 
-                output_file.write(json.dumps(updated_patch) + '\n')
+                output_file.write(json.dumps(updated_patch) + "\n")
             except Exception as e:
                 print("Error:", e)
-        save_metrics(y_true_list, y_pred_list,
-                     input_file_name, model, current_time)
+        save_metrics(y_true_list, y_pred_list, input_file_name, model, current_time)
 
 
 if __name__ == "__main__":
