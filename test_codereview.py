7060c1da6bd8a7dfbe4616a645ab0451fac852c4
Hewlett <yhwi.kwon@gmail.com>
2025-05-20 14:18:19 +0900

    add test codereview

--- test_codereview.py
+++ test_codereview.py
@@ -1,27 +1,45 @@
 import openai
 import json
-import re
 from tqdm import tqdm
 from tqdm.asyncio import tqdm_asyncio
 from datetime import datetime
-from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
-import pandas as pd
 import asyncio
-from nltk.translate.bleu_score import sentence_bleu
-from rouge_score import rouge_scorer
 
 
-def get_classification(model, input_code, primary_category):
+def get_codereview(model, patch):
     # OpenAI API 키 설정
     with open("gpt.key", "r") as key_file:
         api_key = key_file.readline().strip()
     openai.api_key = api_key
     openai.seed = 1115
 
-    # Load the appropriate prompt based on the primary category
-    prompt_file = f"prompt/{primary_category.replace(' ', '_').lower()}.txt"
+    prompt_file = f"prompt/codereview_test.txt"
     prompt = open(prompt_file).read()
+
+    input_code = patch.get("patch") or patch.get("input_code")
+
     cur_prompt = prompt.replace("{{input_code}}", input_code)
+
+    cur_prompt = cur_prompt.replace("{{primary_category}}", patch["primary_category"])
+    cur_prompt = cur_prompt.replace(
+        "{{primary_subcategory}}", patch["primary_subcategory"]
+    )
+    cur_prompt = cur_prompt.replace("{{primary_reason}}", patch["primary_reason"])
+
+    if patch.get("lang"):
+        language_map = {
+            "go": "Go",
+            "java": "Java",
+            "py": "Python",
+            "cpp": "C++",
+            "js": "JavaScript",
+            "rb": "Ruby",
+            "cs": "C#",
+            "c": "C",
+            "php": "PHP",
+        }
+        language = language_map.get(patch["lang"], "")
+        cur_prompt = cur_prompt.replace("{{language}}", language)
 
     response = openai.ChatCompletion.create(
         model=model,
@@ -35,69 +53,47 @@ def get_classification(model, input_code, primary_category):
     # bold markdown 제거
     answer = answer.replace("**", "")
 
-    def extract_info(pattern, text, default="unknown", to_lower=False):
-        match = re.search(pattern, text)
-        if match:
-            result = match.group(1).strip()
-            return result.lower() if to_lower else result
-        return default
+    return answer
 
-    # Extract Type, Reasoning, and Code Review
-    review_type = extract_info(r"Type: (.+)", answer)
-    reasoning = extract_info(r"Reasoning: (.+)", answer)
-    code_review = extract_info(r"Code Review: (.+)", answer)
 
-    return review_type, reasoning, code_review
+def get_vanila_gpt_codereview(model, patch):
+    # OpenAI API 키 설정
+    with open("gpt.key", "r") as key_file:
+        api_key = key_file.readline().strip()
+    openai.api_key = api_key
+    openai.seed = 1115
+
+    prompt = "Write a code review given this code : {{input_code}}"
+    input_code = patch.get("patch") or patch.get("input_code")
+    cur_prompt = prompt.replace("{{input_code}}", input_code)
+
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=[{"role": "user", "content": cur_prompt}],
+        temperature=0.0,
+    )
+
+    # ChatGPT의 응답 내용 추출
+    answer = response["choices"][0]["message"]["content"].strip()
+
+    return answer
 
 
 async def evaluate_patch(model, patch, TOP):
-    input_code = patch["input_code"]
-    primary_category = patch["primary_category"]
+    code_review = await asyncio.to_thread(get_codereview, model, patch)
+    vanila_gpt_codereview = await asyncio.to_thread(
+        get_vanila_gpt_codereview, model, patch
+    )
 
-    review_type, reasoning, code_review = await asyncio.to_thread(
-        get_classification, model, input_code, primary_category
-    )  # IMPORTANT! get_classification 함수를 asyncio.to_thread로 비동기 실행
-
-    patch["review_type"] = review_type
-    patch["reasoning"] = reasoning
     patch["code_review"] = code_review
+    patch["vanila_gpt_codereview"] = vanila_gpt_codereview
 
     return patch
-
-
-def calculate_bleu(reference, hypothesis):
-    reference = [reference.split()]
-    hypothesis = hypothesis.split()
-    bleu_score = sentence_bleu(reference, hypothesis)
-    return round(bleu_score, 4)  # 소수점 4자리까지 반올림
-
-
-def calculate_rouge(reference, hypothesis):
-    scorer = rouge_scorer.RougeScorer(["rouge1", "rougeL"], use_stemmer=True)
-    scores = scorer.score(reference, hypothesis)
-    # 일반적으로 사용하는 rouge1의 fmeasure 값을 반환
-    rouge_score = scores["rougeL"].fmeasure
-    return round(rouge_score, 4)  # 소수점 4자리까지 반올림
 
 
 def save_metrics(patches, input_file_name, model, current_time):
     results = []
     for patch in patches:
-        target = patch.get("target", "")
-        code_review = patch.get("code_review", "")
-        output = patch.get("output", "")
-
-        bleu_target_code_review = calculate_bleu(target, code_review)
-        rouge_target_code_review = calculate_rouge(target, code_review)
-
-        bleu_target_output = calculate_bleu(target, output)
-        rouge_target_output = calculate_rouge(target, output)
-
-        patch["bleu_code_review"] = bleu_target_code_review
-        patch["rouge_code_review"] = rouge_target_code_review
-        patch["bleu_output"] = bleu_target_output
-        patch["rouge_output"] = rouge_target_output
-
         results.append(patch)
 
     with open(
@@ -117,7 +113,12 @@ async def main():
     # model = "o3-mini"
     # model = "o1-mini"
 
-    input_file_name = "df_clnl_4_test3.jsonl_gpt-4o-mini_20250305_020130.jsonl"
+    # input_file_name = "msg-test-1000.jsonl_gpt-3.5-turbo_20250311_022242.jsonl"
+    input_file_name = "msg-test-1000.jsonl_gpt-4o-mini_20250311_020535.jsonl"
+
+    input_file_name = "random_sample_100.jsonl_gpt-4o-mini_20250401_002916.jsonl"
+    # input_file_name = "random_sample_100.jsonl_3.jsonl" #for test
+    input_file_name = "df_clnl_4.jsonl_gpt-4o-mini_20250401_030404.jsonl"
 
     with open(f"data/{input_file_name}", "r") as file:
         patches = [json.loads(line) for line in file]
